642bdd2532a1911ac1bdc2e57f7a91b4557a410a
sujahathmsm <sujahathmhmd@gmail.com>
2024-06-21 07:04:08 +0530

    initialized the Session

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import org.hibernate.Session;
+
+import util.SessionFactoryConfiguration;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Session s = SessionFactoryConfiguration.getInstance().getSession();
     }
 }
---
+++ src/util/SessionFactoryConfiguration.java
@@ -0,0 +1,27 @@
+package util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionFactoryConfiguration {
+    private static SessionFactoryConfiguration sessionFactoryConfiguration;
+
+    private SessionFactory sessionFactory;
+    private SessionFactoryConfiguration(){
+        Configuration configuration = new Configuration().configure();
+        
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static SessionFactoryConfiguration getInstance(){
+        if (sessionFactoryConfiguration == null){
+            sessionFactoryConfiguration = new SessionFactoryConfiguration();
+        }
+        return sessionFactoryConfiguration;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+}
